86d79e51cc26bf28d6743d9e1d54c84926f341ce
ImaginatixnLXIV <luisg2617@gmail.com>
2017-09-16 15:15:09 -0700

    Removed width/height input

--- Main.java
+++ Main.java
@@ -21,36 +21,17 @@ public class Main
   {
 	//Creating instance of Scanner
 	Scanner user_input = new Scanner(System.in);
-	//Creating instance of Window class
-  	Window myWindow = new Window();
 	//Input Verification
 	boolean flag = false;
-	//Width of JFrame window
-	int width = 0;
-	//Height of JFrame window
-	int height = 0;
-	//While loop for window dimensions and input verification
-	while(flag == false)
-	{
-		System.out.println("Input desired resolution for window");
-		System.out.println("Input width");
-	        width = user_input.nextInt();
-		System.out.println("Input height");
-		height = user_input.nextInt();
-		if(height <= 0 || width <= 0)
-		{
-			System.out.println("ERROR: Height or Width not valid");
-			System.out.println("Input again");
-		}//Occurs if either height, width or both are invalid
-		else
-		{
-			flag = true;
-		}//Occurs if input is valid for both width and height
-	}//While loop used for input verfication
-	myWindow.Create(width, height);
 	//Creating instance of GGFrames
-	GGFrames Frames = new GGFrames();
-	Frames.check();
-	myWindow.JLabelCreation(Frames.title());
+      GGFrames Frames = new GGFrames();
+	  //Checking if Dustloop is online
+	  Frames.check();
+	  //Creating title string
+	  String title = "";
+	  //Getting website <title> tag
+	  title = Frames.title();
+	  //Creating instance of Window class
+	  Window myWindow = new Window(title);
   }//End of main method
 }//End of main class
