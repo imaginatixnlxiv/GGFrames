5d28f80a6d7af44cb5117861bf1e1dfb0f40ac0a
ImaginatixnLXIV <luisg2617@gmail.com>
2017-10-02 08:52:17 -0700

    Class that returns Character names and urls

---
+++ Characters.java
@@ -0,0 +1,46 @@
+/**
+ * Class that handles creation and returning of urls
+ *
+ * @author(ImagniatixnLXIV)
+ * @version(10/2/17)
+ */
+public class Characters
+{
+    //Character Name
+    private String char_name;
+    //Character Dustloop url
+    private String char_url;
+
+    /**
+     * Construtor that takes in arguments and sets them
+     * @param name Name of character
+     * @param url Dustloop url for given character
+     * @return none
+     */
+    public Characters(String name, String url)
+    {
+        //Setting passed in arguments to variables in this class
+        char_name = name;
+        char_url = url;
+    }
+
+    /**
+     * Returns String char_url
+     * @param none
+     * @return char_url Character Dustloop Url
+     */
+    public String getUrl()
+    {
+        return char_url;
+    }//End of method getUrl
+
+    /**
+     * Returns String char_name
+     * @param none
+     * @return char_name Character Name
+     */
+    public String getName()
+    {
+        return char_name;
+    }//End of method getName
+}
