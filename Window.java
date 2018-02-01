ee6037f4c9f715133eaf2db70309bda716d85ab4
unknown <imaginatixnlxiv>
2018-02-01 08:32:15 -0800

    Working on formatting Window layout

--- Window.java
+++ Window.java
@@ -1,3 +1,4 @@
+
 /**
  * Class that JFrame functions will take place in
  *
@@ -13,138 +14,130 @@ import java.util.ArrayList;
 
 /**
  * Class that handles all JFrame Functions
- * */
+ */
 public class Window {
-    //Creating instance of JFrame
-    JFrame mainWindow;
-    ArrayList <Characters> WindowData;
-    public Window(ArrayList <Characters> list)
-    {
-        obtainData(list);
-        CreateMainWindow();
-    }
-    public void obtainData(ArrayList <Characters> myList)
-    {
-        WindowData = myList;
+	// Creating instance of JFrame
+	JFrame mainWindow;
+	ArrayList<Characters> WindowData;
 
-    }
-    /**
-     * Method that creates window
-     * @param none
-     * @return none
-     * */
-    private void CreateMainWindow() {
-        //Creating JFrame
-        mainWindow = new JFrame("GGFrames");//Creates instnace of JFrame and assigns window name
-        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Close operation
-        mainWindow.setSize(800, 600);//Determines size of window
+	public Window(ArrayList<Characters> list) {
+		obtainData(list);
+		CreateMainWindow();
+	}
 
-        mainWindow.setLocationRelativeTo(null);//Opens window in the center of the screen
-        mainWindow.setVisible(true);//Displays window
-        mainWindow.getContentPane().setBackground(Color.black);
-        MainMenuCreation();//Creates Buttons for Character Menu
-    }//End of create method
+	public void obtainData(ArrayList<Characters> myList) {
+		WindowData = myList;
+	}
 
-    /**
-     * Clears Screen then calls either create frame data or returns to main menu
-     * @param mode Determines weather to call CreateMainWindow method or CreateFrameData method
-     * @param char_selected Determines what character was clicked
-     * @return none
-     **/
-    private void ClearScreen(boolean mode, int char_selected)
-    {
-        //Clearing screen
-        mainWindow.removeAll();
-        mainWindow.revalidate();
-        //Calling either CreateFrameData or CreateMainWindow method
-        int character = char_selected;
-        if(mode == false)
-        {
-            //CreateFrameData(character);
-        }//Creates FrameData table
-        else
-        {
-            CreateMainWindow();
-        }//Returns to main menu
-    }//End of ClearScreen method
+	/**
+	 * Method that creates window
+	 * 
+	 * @param none
+	 * @return none
+	 */
+	private void CreateMainWindow() {
+		// Creating JFrame
+		mainWindow = new JFrame("GGFrames");// Creates instnace of JFrame and assigns window name
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Close operation
+		mainWindow.setSize(1280, 800);// Determines size of window
+		mainWindow.setLayout(new BorderLayout());
+		mainWindow.setLocationRelativeTo(null);// Opens window in the center of the screen
+		mainWindow.setVisible(true);// Displays window
+		mainWindow.getContentPane().setBackground(Color.black);
+		MainMenuCreation();// Creates Buttons for Character Menu
+	}// End of create method
 
-    /**
-     * method that creates quitButton
-     * @param none
-     * @return none
-     */
-    private void quitButton()
-    {
-        //Creating Bottom Menu Jpanel
-        JPanel bot_menu = new JPanel(new GridBagLayout());
-        bot_menu.setPreferredSize(new Dimension(100,100));
-        //Creating exit button
-        JButton Quit_button = new JButton("Quit");
-        //Setting dimensions for quit button
-        Quit_button.setPreferredSize(new Dimension(70,70));
-        //Styling Quit Button
-        Quit_button.setBackground(Color.red);
-        //Adding click action to quit button
-        Quit_button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                mainWindow.dispose();
-            }
-        });
-        //Adding buttons to Jpanel
-        bot_menu.add(Quit_button);
-        mainWindow.add(bot_menu, BorderLayout.SOUTH);
-    }
-    /**
-     * Creates buttons
-     * @param none
-     * @return none
-     */
-    public void MainMenuCreation() {
-        //Creating GridBagConstraints
-        GridBagConstraints c = new GridBagConstraints();
-        //Creating label Chara_menu
-        JLabel chara_menu = new JLabel("Character Select", JLabel.CENTER);
-        //Constants for button size
-        final int BUTTON_HEIGHT = 100;
-        final int BUTTON_WIDTH = 100;
-        //Creating JPanel character_select
-        JPanel character_select = new JPanel(new GridBagLayout());
-        //Adding JLabel to character_select JPanel
-        //character_select.add(main_label);
-        //character_select.add(chara_menu);
-        //Creating imageicon
-        //ImageIcon BaikenIcon = new ImageIcon("Baiken.png");
-        //Creating character buttons
-        JButton Answer_button = new JButton(WindowData.get(0).getName());
-        JButton Axl_button = new JButton(WindowData.get(1).getName());
-        JButton Baiken_button = new JButton(WindowData.get(2).getName());
-        JButton Bedman_button = new JButton(WindowData.get(3).getName());
-        JButton Chipp_button = new JButton(WindowData.get(4).getName());
-        JButton Dizzy_button = new JButton(WindowData.get(5).getName());
-        JButton Elphelt_button = new JButton(WindowData.get(6).getName());
-        JButton Faust_button = new JButton(WindowData.get(8).getName());
-        JButton INo_button = new JButton(WindowData.get(9).getName());
-        JButton JackO_button = new JButton(WindowData.get(10).getName());
-        //Click Action for Character Buttons
-        Baiken_button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                ClearScreen(false,2);//Calls ClearScreen method passing in false which means create frame data window
-            }
-        });
-        //Setting Dimensions of button
-        Baiken_button.setPreferredSize(new Dimension(200,200));
-        //Decorating buttons
-        Baiken_button.setBackground(Color.black);
-        Baiken_button.setForeground(Color.white);
-        //Adding buttons to panel
-        character_select.add(Answer_button);
-        character_select.add(Axl_button);
-        character_select.add(Baiken_button);
-        //Adding panel to JFrame
-        quitButton();//Calls method to create quit button
-        mainWindow.add(character_select);
-        mainWindow.revalidate();
-    }
-}//End of class Window
+	/**
+	 * Clears Screen then calls either create frame data or returns to main menu
+	 * 
+	 * @param mode
+	 *            Determines weather to call CreateMainWindow method or
+	 *            CreateFrameData method
+	 * @param char_selected
+	 *            Determines what character was clicked
+	 * @return none
+	 **/
+	private void ClearScreen(boolean mode, int char_selected) {
+		// Clearing screen
+		mainWindow.removeAll();
+		mainWindow.revalidate();
+		// Calling either CreateFrameData or CreateMainWindow method
+		int character = char_selected;
+		if (mode == false) {
+			// CreateFrameData(character);
+		} // Creates FrameData table
+		else {
+			CreateMainWindow();
+		} // Returns to main menu
+	}// End of ClearScreen method
+
+	/**
+	 * method that creates quitButton
+	 * 
+	 * @param none
+	 * @return none
+	 */
+	private void quitButton() {
+		// Creating Bottom Menu Jpanel
+		JPanel bot_menu = new JPanel(new GridBagLayout());
+		bot_menu.setPreferredSize(new Dimension(100, 100));
+		// Creating exit button
+		JButton Quit_button = new JButton("Quit");
+		// Setting dimensions for quit button
+		Quit_button.setPreferredSize(new Dimension(70, 30));
+		// Styling Quit Button
+		Quit_button.setBackground(Color.red);
+		// Adding click action to quit button
+		Quit_button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent actionEvent) {
+				mainWindow.dispose();
+			}
+		});// End of ActionListener
+			// Adding buttons to Jpanel
+		bot_menu.add(Quit_button);
+		mainWindow.add(bot_menu, BorderLayout.SOUTH);
+	}// End of method quitButton
+
+	/**
+	 * Creates buttons
+	 * 
+	 * @param none
+	 * @return none
+	 */
+	public void MainMenuCreation() {
+		// Creating label Chara_menu
+		JLabel chara_menu = new JLabel("Character Select", JLabel.CENTER);
+		//Creating JPanel title
+		JPanel menu_title = new JPanel(new GridLayout(1,1));
+		menu_title.setPreferredSize(new Dimension(200,800));
+		//Addint JLabel chara_menu to JPanel menu_title
+		menu_title.add(chara_menu);
+		// Creating JPanel character_select
+		JPanel character_select = new JPanel();
+		character_select.add(chara_menu);
+		//Grid dimensions
+		final int GRID_ROW = 5;//Number of rows in gridlayout
+		final int GRID_COL = 5;//Number of columns in gridlayout
+		final int GRID_HGAP = 10;//Hgap size in gridlayout
+		final int GRID_WGAP = 10;//Wgap size in gridlayout
+		// Setting layout for JPanel character_select to GridBagLayout
+		character_select.setLayout(new GridLayout(GRID_ROW,GRID_COL,GRID_HGAP,GRID_WGAP));
+		// ImageIcon BaikenIcon = new ImageIcon("Baiken.png");
+		// Creating character buttons
+		ArrayList<JButton> Char_buttons = new ArrayList<JButton>();
+		// Incrementor for while loop
+		int d = 0;
+		// While loop to add JButtons to JPanel
+		while (d < WindowData.size()) {
+			System.out.println("We in here");// Testing output
+			Char_buttons.add(new JButton(WindowData.get(d).getName()));// Adding JButton to ArrayList Char_buttons
+			character_select.add(Char_buttons.get(d));// Adding JButtons to JPanel
+			d++;// Incrementing
+		} // End of while loop
+		quitButton();// Calls method to create quit button
+		mainWindow.add(menu_title, BorderLayout.CENTER);//Adding JPanel
+		mainWindow.add(character_select);// Adding JPanel
+		mainWindow.revalidate();// Revalidating JFrame
+	}// End of MainMenuCreation method
+}// End of class Window
