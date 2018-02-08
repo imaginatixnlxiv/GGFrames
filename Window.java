040a72a4ac4c05120a8658d107251848a8225f67
unknown <imaginatixnlxiv>
2018-02-08 08:50:45 -0800

    Major formatting changes

--- Window.java
+++ Window.java
@@ -103,6 +103,7 @@ public class Window {
 		});// End of ActionListener
 			// Adding buttons to Jpanel
 		bot_menu.add(Quit_button);
+		bot_menu.setBackground(Color.BLACK);
 		mainWindow.add(bot_menu, BorderLayout.SOUTH);
 	}// End of method quitButton
 
@@ -113,36 +114,51 @@ public class Window {
 	 * @return none
 	 */
 	public void MainMenuCreation() {
-		// Creating label Chara_menu
-		JLabel chara_menu = new JLabel("Character Select", SwingConstants.CENTER);
-		//Creating JPanel title
-		JPanel menu_title = new JPanel(new BorderLayout());
-		menu_title.setPreferredSize(new Dimension(200,800));
-		//Addint JLabel chara_menu to JPanel menu_title
-		menu_title.add(chara_menu);
 		// Creating JPanel character_select
 		JPanel character_select = new JPanel();
-		character_select.add(chara_menu);
+		character_select.setBackground(Color.black);
 		//Grid dimensions
 		final int GRID_ROW = 5;//Number of rows in gridlayout
 		final int GRID_COL = 5;//Number of columns in gridlayout
 		final int GRID_HGAP = 10;//Hgap size in gridlayout
 		final int GRID_WGAP = 10;//Wgap size in gridlayout
 		// Setting layout for JPanel character_select to GridBagLayout
-		character_select.setLayout(new GridLayout(GRID_ROW,GRID_COL,GRID_HGAP,GRID_WGAP));
+		character_select.setLayout(new GridBagLayout());
+		//GridBag Constraits
+		GridBagConstraints Constraints = new GridBagConstraints();
+		Constraints.gridheight = 1;
+		Constraints.gridwidth = 1;
+		Constraints.weightx = .0000001;
+		Constraints.weighty = .0000001;
 		// ImageIcon BaikenIcon = new ImageIcon("Baiken.png");
 		// Creating character buttons
 		ArrayList<JButton> Char_buttons = new ArrayList<JButton>();
 		// Incrementor for while loop
 		int d = 0;
-		// While loop to add JButtons to JPanel
+		// While loop to adding JButtons to arraylist Char_buttons
 		while (d < WindowData.size()) {
 			Char_buttons.add(new JButton(WindowData.get(d).getName()));// Adding JButton to ArrayList Char_buttons
-			character_select.add(Char_buttons.get(d));// Adding JButtons to JPanel
+			Char_buttons.get(d).setPreferredSize(new Dimension(200,100));
 			d++;// Incrementing
 		} // End of while loop
+		//For loop for adding ArrayList of JButtons to JPanel
+		int inc = 0;//Incrementor for arraylist
+		for(int col = 0; col < GRID_COL; col++)
+		{
+			Constraints.gridy = col;//Setting Column
+			for(int row = 0; row < GRID_ROW; row++)
+			{
+				Constraints.gridx = row;//Setting row
+				character_select.add(Char_buttons.get(inc), Constraints);//Adding button to specified column and row in JPanel
+				inc++;//Incrementing Button ArrayList
+			}//End of inner for loop
+		}//End of outer for loop
+		//For loop for setting color for buttons
+		for(JButton colorize : Char_buttons)
+		{
+			colorize.setBackground(Color.decode("#1ca2db"));//Setting color to buttons as grey
+		}
 		quitButton();// Calls method to create quit button
-		mainWindow.add(menu_title, BorderLayout.CENTER);//Adding JPanel
 		mainWindow.add(character_select);// Adding JPanel
 		mainWindow.revalidate();// Revalidating JFrame
 	}// End of MainMenuCreation method
